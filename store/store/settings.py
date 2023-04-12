c26210ce82fe4960341c175fb5ea72ed27e4e89f
shayansiddiqui759 <83961256+shayansiddiqui759@users.noreply.github.com>
2023-04-12 14:10:57 +0500

    add product module (#3)

--- store/store/settings.py
+++ store/store/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+LOCAL_APPS = [
+    'product',
+]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
